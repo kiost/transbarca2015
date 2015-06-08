74f7a44ce1da393af1f56f703ab40b4f0fc3b9f9
Mauricio Bautista <kiost1994@gmail.com>
2015-06-08 16:02:38 -0430

    Primer
    Mauricio

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,41 @@
+<html>
+	<head>
+		<title>Laravel</title>
+		
+		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+		{!! Html::style('bower_components/bootstrap/dist/css/bootstrap.min.css') !!}
+		{!! Html::style('bower_components/bootstrap-material-design/dist/css/material.min.css') !!}
+		{!! Html::style('bower_components/bootstrap-material-design/dist/css/ripples.min.css') !!}
+		{!! Html::style('bower_components/bootstrap-material-design/dist/css/roboto.min.css') !!}
+		{!! Html::style('bower_components/bootstrap-material-design/dist/css/material-fullpalette.min.css') !!}
+<style>
+	body{
+		background-color: #B7011C;
+	}
+
+</style>
+	</head>
+	<body>
+<div class=" container" style="height:50em">
+<div class="col-md-12 well" style="margin-top:10px;height:80px ">
+	<div class=" col-md-6" >dasdasd</div>
+	<div class="col-md-6">baasdasdsadasdrra de navegacion</div>
+</div>
+	<section class="col-md-12">
+		<article class="col-md-8 well" style="height:40em">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, officia veniam explicabo eveniet incidunt quisquam sequi debitis, quos eaque minima. Quod culpa beatae obcaecati ipsam, ducimus nihil voluptates, voluptatem expedita!
+		</article>
+		<aside class="col-md-3 well" style="float:right ; height:40em">twitter</aside>
+	</section>
+
+
+
+	<footer class="col-md-12 well">
+		Diseñado Por Pasantes
+	</footer>
+	</div>	
+	
+
+	</body>
+</html>
